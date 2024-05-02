038bdfdf55d6a2328d515581552f449f915d631d
Dhivya <dvya1425@gmail.com>
2024-05-02 18:34:03 +0530

    custom navigation bar

--- free_up/lib/Utils/CustomWidgets/CustomBottomNavigation.dart
+++ free_up/lib/Utils/CustomWidgets/CustomBottomNavigation.dart
@@ -61,100 +61,96 @@ showPinBottomSheet(BuildContext context) async {
 }
 }
 
-/*class CustomBottomNavigationBar extends StatefulWidget {
-  final List icon;
-  final List label;
-  final double heigth;
-  final Color color;
-  final Color backgroundColor;
-  final BoxFit fit;
-  final ValueChanged<int>? onTabSelected;
-  final dynamic? commonViewModel;
-
-  CustomBottomNavigationBar(
-      {Key? key,
-      required this.icon,
-      required this.label,
-      required this.heigth,
-      required this.color,
-      required this.backgroundColor,
-      this.onTabSelected,
-      this.commonViewModel,
-      required this.fit})
-      : super(key: key);
-
-  @override
-  State<CustomBottomNavigationBar> createState() =>
-      _CustomBottomNavigationBarState();
-}
-
-class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
-  BottomNavigationBarItem buildBottomNavBarItems(String icon, String label) {
-    Image imageActive = Image.asset(
-      icon,
-      height: widget.heigth + 5,
-      fit: BoxFit.fill,
-    );
-    Image image = Image.asset(
-      icon,
-      height: widget.heigth + 5,
-      fit: BoxFit.fill,
-      opacity: const AlwaysStoppedAnimation(.3),
-    );
-
-    return BottomNavigationBarItem(
-      icon: Padding(
-        padding: const EdgeInsets.all(0),
-        child: image,
-      ),
-      activeIcon: Padding(
-        padding: const EdgeInsets.all(0),
-        child: imageActive,
-      ),
-      label: label,
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    _updateIndex(int index) {
-      widget.onTabSelected!(index);
-    }
-
-    buildItems() {
-      List<BottomNavigationBarItem> list = [];
-      for (int i = 0; i < widget.icon.length; i++) {
-        list.add(buildBottomNavBarItems(widget.icon[i], widget.label[i]));
-      }
-      return list;
-    }
-
-    return BottomNavigationBar(
-        backgroundColor: checkBrightness.value == Brightness.dark
-            ? backgroundHardDarkThemeColor
-            : white,
-        elevation: 0,
-        unselectedFontSize: 12,
-        selectedFontSize: 12,
-        selectedLabelStyle: const TextStyle(
-          fontFamily: 'GoogleSans',
-          fontWeight: FontWeight.w500,
-        ),
-        unselectedLabelStyle: const TextStyle(
-          fontFamily: 'GoogleSans',
-          fontWeight: FontWeight.w500,
-        ),
+/*BottomNavigationBar(
+        onTap: (newIndex) => setState(() => _index = newIndex),
+        currentIndex: _index,
+        unselectedItemColor: CustomColors.grey,
+        selectedItemColor: CustomColors.appTheme,
         type: BottomNavigationBarType.fixed,
-        unselectedItemColor: themeSupport().isSelectedDarkMode()
-            ? white
-            : black,
-        selectedItemColor: themeSupport().isSelectedDarkMode()
-            ? backgroundGreenThemeColor
-            : backgroundDarkThemeColor,
-        currentIndex: widget.commonViewModel!.id,
-        showUnselectedLabels: true,
-        items: buildItems(),
-        onTap: _updateIndex
-    );
-  }
-}*/
+        items: [
+          BottomNavigationBarItem(
+              icon: ImageIcon(
+                AssetImage(walletList),
+              ),
+              label: 'Wallets'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                Security1,
+                height: 25,
+                fit: BoxFit.fill,
+                color: Colors.grey,
+              ),
+              activeIcon: SvgPicture.asset(
+                Security1,
+                height: 25,
+                fit: BoxFit.fill,
+                color: CustomColors.buttonColor,
+              ),
+              label: 'Security'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 0,
+              ),
+              label: ''),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                FilterB,
+                height: 25,
+                fit: BoxFit.fill,
+                color: Colors.grey,
+              ),
+              /*  icon: ImageIcon(
+                        AssetImage(filter),
+                      ),*/
+              activeIcon: SvgPicture.asset(
+                FilterB,
+                height: 25,
+                fit: BoxFit.fill,
+                color: CustomColors.buttonColor,
+              ),
+              label: 'Filter'),
+          BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                prefer,
+                height: 25,
+                fit: BoxFit.fill,
+                color: Colors.grey,
+              ),
+              activeIcon: SvgPicture.asset(
+                prefer,
+                height: 25,
+                fit: BoxFit.fill,
+                color: CustomColors.buttonColor,
+              ),
+              label: 'Settings'),
+        ],
+        selectedLabelStyle: TextStyle(fontSize: 12),
+      ),
+      floatingActionButton: Visibility(
+        visible: !keyboardIsOpen,
+        child: Container(
+          height: 90,
+          width: 70,
+          child: FloatingActionButton(
+            onPressed: () {
+              (newIndex) => setState(() => _index = newIndex);
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => PaymentWallet(
+                          context,
+                        )),
+              );
+            },
+            backgroundColor: CustomColors.appTheme,
+            child: SvgPicture.asset(
+              WalletBlk,
+              height: 28,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,*/
